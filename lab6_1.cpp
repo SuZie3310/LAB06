4c9bfce73cd2ebdef69a2265b7b928c2a230f325
SuZie3310 <Surapat5163@gmail.com>
2025-12-09 15:48:39 +0700

    lab6 แก้ข้อ1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int number;
+    int x=0;
+    int y=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number;
+    while (number!=0)
+    {
+        
+    if (number%2 ==0)
+    {
+        x+=1;
+    }
+    else{
+        y+=1;
+    }
+    cout << "Enter an integer: ";
+    cin >> number;
+    }
+    cout << "#Even numbers = "<< x <<"\n";
+    cout << "#Odd numbers = "<< y <<"\n";
     return 0;
 }
